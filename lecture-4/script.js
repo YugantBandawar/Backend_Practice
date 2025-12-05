f794550a4108c233e28fab0b208f49a0fb27e1b5
YugantBandawar <137055727+YugantBandawar@users.noreply.github.com>
2025-12-05 20:36:59 +0530

    Express Routing and middleware

---
+++ lecture-4/script.js
@@ -0,0 +1,25 @@
+const express= require('express');
+
+const app = express();
+
+app.use(function(req,res,next){
+    console.log("middleware run");
+    next();
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+})
+app.get('/profile', (req, res) => {
+  //res.send('profile page hai ye');
+  return next(new Error("Not implemented"))
+})
+
+app.use((err,req,res,next)=>{
+    console.error(err.stack)
+    res.status(500).send('Something broke')
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
