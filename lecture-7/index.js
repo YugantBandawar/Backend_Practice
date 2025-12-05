ced4c517c3e95324a9b3bce628abb0eabb53ee5e
YugantBandawar <137055727+YugantBandawar@users.noreply.github.com>
2025-12-06 01:37:54 +0530

    Lecture 8 new feature added

--- lecture-7/index.js
+++ lecture-7/index.js
@@ -1,25 +1,46 @@
-const express=require('express');
-const app=express();
-const path=require('path');
-const fs=require('fs');
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,'public')));
-app.set('view engine','ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
-app.get('/',function(req,res){
-    fs.readdir(`./files`,function(err,files){
-     res.render("index",{files:files}) ;
-    })
-   
-})
+app.get('/', function (req, res) {
+  fs.readdir('./files', function (err, files) {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Error reading files folder");
+    }
+    res.render("index", { files: files });
+  });
+});
 
-app.post('/create',function(req,res){
-   fs.writeFile(`./files/${req.body.title.split('') .join('')}.txt`,req.body.details,function(err){
-     res.redirect("/")
+app.get('/file/:filename', function (req, res) {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", function (err, filedata) {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Error reading file");
+    }
 
-   });
-   
-})
-app.listen(3000);
+    res.render('show', {
+      filename: req.params.filename,
+      filedata: filedata,
+    });
+  });
+});
+
+app.post('/create', function (req, res) {
+  const safeTitle = req.body.title.split(' ').join('');
+  fs.writeFile(`./files/${safeTitle}.txt`, req.body.details, function (err) {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Error creating file");
+    }
+    res.redirect("/");
+  });
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
