72f75e78f66a67d719e8bba07dffcf08fe4531db
YugantBandawar <137055727+YugantBandawar@users.noreply.github.com>
2025-12-06 16:07:09 +0530

    Authentication and Authorisation

---
+++ lecture-14/app.js
@@ -0,0 +1,25 @@
+const cookieParser=require('cookie-parser');
+const express=require("express");
+const app=express();
+const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
+
+app.use(cookieParser());
+
+app.get("/",function(req,res){
+
+   let token= jwt.sign({email:"test@gmail.com"},"secret");
+   res.cookie("token",token)
+   console.log(token);
+   res.send("done")
+    
+})
+
+app.get("/read",function(req,res){
+    let data=jwt.verify(req.cookies.token,"secret");
+    console.log(data);
+})
+
+
+
+app.listen(3000);
